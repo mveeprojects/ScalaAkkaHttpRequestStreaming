eab250fd4b640bf88a724c3d16ffbdfc31dd049d
mva13 <markvallis1987@Gmail.com>
2021-02-10 11:09:23 +0000

    Added catch-all case to item actor

--- src/main/scala/actors/AppActor.scala
+++ src/main/scala/actors/AppActor.scala
@@ -17,5 +17,6 @@ class ItemActor extends AppActor {
   def receive: Receive = {
     case AddItem(itemId) => logger.info(s"Adding $itemId")
     case RemoveItem(itemId) => logger.info(s"Removing $itemId")
+    case _ => logger.error("Invalid input provided")
   }
 }
